62f4e7cd8595afbba5190e1fb663ed239f670383
= <pbale95@gmail.com>
2016-03-23 15:32:06 +0100

    Some commments

--- sim.py
+++ sim.py
@@ -13,10 +13,14 @@ padding = 20
 inner_padding = 5
 hidden_color = "red"
 found_color = "green"
-bot_color = "blue"
+robot_color = "blue"
+
+object_marker = 10
 
 start_row = 3
 start_col = 2
+
+robot_pos = (start_row, start_col)
 
 def draw_grid(grid): 
 
@@ -25,16 +29,23 @@ def draw_grid(grid):
     print "Width:", canvas['width'], "Height:", canvas['height']
     print "Rows:", rows, ", Cols:", cols
 
+    # Draw lines
     for y in range(rows + 1):
         canvas.create_line(padding, y * cell_scale + padding, cols*cell_scale + padding, y * cell_scale + padding)
     for x in range(cols + 1):
         canvas.create_line(x * cell_scale + padding, padding, x * cell_scale + padding, rows * cell_scale + padding)
 
+
+    # Draw marked objects
     for i in range(rows):
         for j in range(cols):
-            if (grid[i,j] == True):
+            if (grid[i,j] == object_marker):
                 draw_cell(i, j, hidden_color)
             draw_cell_label(i, j)
+
+    # Draw robot
+    draw_cell(robot_pos[0], robot_pos[1], robot_color)
+    draw_cell_label(robot_pos[0], robot_pos[1])
 
 def draw_cell_label(row, col):
     row_offset = row * cell_scale + padding + inner_padding + cell_scale / 2
@@ -64,38 +75,42 @@ def randomize_grid(grid):
             rand_col += 1
 
         if (grid[rand_row, rand_col] == False):
-            grid[rand_row, rand_col] = True
+            grid[rand_row, rand_col] = object_marker
+            num_objects -= 1
 
-        num_objects -= 1
-
-    if not reachable(grid):
+    if not reachable(grid): # re-run if needed
         randomize_grid(grid)
 
 def reachable(grid):
     rows, cols = grid.shape
     grid2 = np.mat([[0 for x in range(cell_width)] for x in range(cell_height)])
-    grid2[:] = grid
+    grid2[:] = grid #deep copy so we can mangle
 
-    traverse(grid2, start_row, start_col)
+    traverse(grid2, start_row, start_col) # depth first recursive traversal
+
+    # check if we hit traversed everything
     for i in range(rows):
         for j in range(cols):
-            if grid2[i,j] == False or grid2[i,j] == 1:
+            if grid2[i,j] == False or grid2[i,j] == object_marker:
                 return False
 
     return True
 
 def traverse(grid, row, col):
+    spot_marked = 2
+
     if (row < 0 or row > cell_height - 1):
         return
     if (col < 0 or col > cell_width - 1):
         return
 
-    if grid[row, col] == 2 or grid[row, col] == 1 or grid[row, col] == 3:
-        if grid[row, col] == 1:
-            grid[row, col] = 3
+    if grid[row, col] == spot_marked or grid[row, col] == object_marker:
+        if grid[row, col] == object_marker:
+            grid[row, col] = spot_marked
+
         return
 
-    grid[row, col] = 2
+    grid[row, col] = spot_marked
 
     traverse(grid, row, col + 1)
     traverse(grid, row, col - 1) 
@@ -116,7 +131,7 @@ grid = np.mat([[0 for x in range(cell_width)] for x in range(cell_height)])
 randomize_grid(grid)
 
 draw_grid(grid)
-draw_cell(3,2, bot_color)
+
 canvas.pack()
 
 atexit.register(root.mainloop)
